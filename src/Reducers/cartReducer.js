aaf096ef6b3773cbd577b13da5f73ade7e9832cc
Jake <jakefrisbee@gmail.com>
2020-09-16 17:33:11 -0700

    saved cart to session storage

--- src/Reducers/cartReducer.js
+++ src/Reducers/cartReducer.js
@@ -2,9 +2,27 @@ import {
     ADD
 } from '../actions/actionTypes';
 
-const cartReducer = (state={}, action) => {
+let INITIAL_STATE = {}
+const cart = JSON.parse(sessionStorage.getItem('cart'));
+if (cart) {
+    for (let item of cart){
+        INITIAL_STATE[item._id] = item
+    }
+}
+
+const cartReducer = (state=INITIAL_STATE, action) => {
+    console.log(state)
     switch(action.type) {
         case ADD:
+            let storage = JSON.parse(sessionStorage.getItem('cart'));
+            if (storage) {
+                storage.push(action.item);
+                sessionStorage.setItem('cart', JSON.stringify(storage))
+            }
+            else {
+                sessionStorage.setItem('cart', JSON.stringify([action.item]))
+            }
+      
             return {...state, [action.id]: action.item }
     }
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import '../styles/App.css';
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
+    const items = useSelector(store => store) || {}
+
+    console.log(items)
     return (
         <div className="Cart">
+            <div className="Cart-Container">
+                <ul>
+                    {Object.keys(items).map(item =>
+                        <li className="Cart-Item">
+                            <div className="Cart-Details">
+                                <img src={`http://127.0.0.1:5000/static/images/${items[item].img}`} alt={item.img} />
+                            </div>
+                            <div className="Cart-Quantity">
+
+                            </div>
+                            <div className="Cart-Price">
+                                <div>{items[item].price}</div>
+                            </div>
+                        </li>
+                    )}
+                </ul>
+      
+            </div>
         </div>
     );
 }
