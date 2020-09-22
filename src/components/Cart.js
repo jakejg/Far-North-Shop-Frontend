0f937c011e630939a928c524bdecefb73a97ea1b
Jake <jakefrisbee@gmail.com>
2020-09-21 17:18:47 -0700

    add form to change quantity of each item in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
 import { useSelector } from 'react-redux';
 import '../styles/Cart.css';
+import { Button } from 'reactstrap';
+
+function createFormState(items){
+    const form = {}
+    for (let item of Object.keys(items)){
+        form[item] = items[item].quantity
+    }
+    return form;
+}
 
 const Cart = () => {
     const items = useSelector(store => store) || {};
+    const INITIAL_FORM_STATE = createFormState(items);
+    const [formData, setFormData] = useState(INITIAL_FORM_STATE);
+    console.log(formData)
 
     const getTotalPrice = () => {
         let total = 0;
@@ -14,21 +26,26 @@ const Cart = () => {
         return total;
     }
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(formData => ({...formData, [name]: value}))
+    }
+
     return (
         <div className="Cart">
             <div className="Cart-Container">
                 <ul>
                     {Object.keys(items).map(item =>
-                        <li className="Cart-Item">
+                        <li key={item} className="Cart-Item">
                             <div>
                              <img className="Cart-Image" src={`http://127.0.0.1:5000/static/images/${items[item].img}`} alt={item.img} />
                              </div>
                             <div className="Cart-Details">
                                <div className="Cart-Text">{items[item].name}</div>
                             </div>
-                            <div className="Cart-Quantity">
-                                <div className="Cart-Text">{items[item].quantity}</div>
-                            </div>
+                            <form className="Cart-Quantity">
+                                    <input className="Cart-Quantity-Inupt" type="number" min="0" onChange={handleChange} value={formData[item]} name={item} />
+                            </form>
                             <div className="Cart-Price">
                                 <div className="Cart-Text">${items[item].price}</div>
                             </div>
@@ -38,7 +55,8 @@ const Cart = () => {
                         <div className="total">Total ${getTotalPrice()} </div>
                     </li>
                 </ul>
-      
+                <Button >Update Cart</Button>
+                <Button>Checkout</Button>
             </div>
         </div>
     );
